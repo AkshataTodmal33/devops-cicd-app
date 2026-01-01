eefc3a0fdab1d6d25968ab4d5f1847606de5c31d
Ubuntu <ubuntu@ip-172-31-18-38.ec2.internal>
2026-01-01 16:25:39 +0000

    Test CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "DevOps CI/CD Pipeline Working Successfully!"
+    return "DevOps CI/CD Pipeline Test Successfully!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
